741da772b35714051ab62eb3cf6f7ca2fb1cd822
Fatih Karahan <hseyin97@gmail.com>
2020-07-30 20:18:08 +0300

    polymorphised the error classes

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,10 +1,19 @@
+import { CustomError } from "./custom-error";
 import { ValidationError } from "express-validator";
 
-export class RequestValidationError extends Error {
+export class RequestValidationError extends CustomError {
+  statusCode = 400;
+
   constructor(public errors: ValidationError[]) {
-    super();
+    super("Invalid request parameters");
 
     // only because this class is extending a built-in class
     Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
+
+  serializeErrors() {
+    return this.errors.map((err) => {
+      return { message: err.msg, field: err.param };
+    });
   }
 }
