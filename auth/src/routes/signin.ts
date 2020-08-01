baf13c97af0fc66ec8cc443c17bfa67fc77ed613
Fatih Karahan <hseyin97@gmail.com>
2020-08-01 21:27:31 +0300

    implemented the signin route

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from "express";
+import jwt from "jsonwebtoken";
 import { body } from "express-validator";
 import { validateRequest } from "../middlewares/validate-request";
+import { User } from "../models/user";
+import { BadRequestError } from "../errors/bad-request-error";
+import { Password } from "../services/password";
 
 const router = express.Router();
 
@@ -14,7 +18,36 @@ router.post(
       .withMessage("A password must be supplied"),
   ],
   validateRequest,
-  (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    const passwordMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+    if (!passwordMatch) {
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    const userJWT = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    req.session = {
+      jwt: userJWT,
+    };
+
+    res.status(200).send(existingUser);
+  }
 );
 
 export { router as signinRouter };
