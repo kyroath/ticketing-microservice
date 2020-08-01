8e8cfa8348d2491de980c8575f27ba4452aa9d85
Fatih Karahan <hseyin97@gmail.com>
2020-08-01 21:17:46 +0300

    moved request validation to a common middleware

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -6,6 +6,7 @@ import { User } from "../models/user";
 
 import { RequestValidationError } from "../errors/request-validation-error";
 import { BadRequestError } from "../errors/bad-request-error";
+import { validateRequest } from "../middlewares/validate-request";
 
 const router = express.Router();
 
@@ -18,13 +19,8 @@ router.post(
       .isLength({ min: 4, max: 64 })
       .withMessage("Password must be between 4 and 64 characters"),
   ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-
     const { email, password } = req.body;
     const existingUser = await User.findOne({ email });
 
